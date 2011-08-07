887c2218537027ea1b6f4ee5b1eef56dcb79a622
Piotr Kaleta <piotrek.kaleta@gmail.com>
2011-08-07 19:04:17 +0200

    Codeforces #80

---
+++ 80/d/d.py
@@ -0,0 +1,20 @@
+n, m, p = map(int, sys.stdin.readline().split())
+
+ret = []
+
+for i in xrange(p):
+    x = int(sys.stdin.readline())
+
+    if k >= n/2:
+        if x <= n-k:
+            ret.append('.')
+        else
+            ret.append('X')
+    elif n % 2 != 0:
+
+    elif n % 2 == 0:
+        if x % 2 == 0 and x > n-(2*k-1):
+            ret.append('X')
+        else:
+            ret.append('.')
+
